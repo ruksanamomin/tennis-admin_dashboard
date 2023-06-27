8168d7088080d941e9c355be45f3d959501182d5
Balajisaketh <70933350+Balajisaketh@users.noreply.github.com>
2023-06-27 12:22:18 +0530

    done
    done

--- src/Pages/Modals/courtmodal.tsx
+++ src/Pages/Modals/courtmodal.tsx
@@ -1,55 +1,61 @@
 import React ,{useState}from 'react'
 
 import { Avatar, Button, Checkbox, Label, Modal, TextInput } from 'flowbite-react';
+import axios from 'axios';
 
 function CourtModal() {
   const [openModal, setOpenModal] = useState("");
   const props = { openModal, setOpenModal };
   const [selectedFile, setSelectedFile] = useState(null);
-//   const [avatar,setAvatar] = useState("");
+  const [avatar,setAvatar] = useState("");
 
-//    const handleFileChange = (e) => {
-//       setAvatar(e.target.files[0]);
-//    };
+   const handleFileChange = (e:any) => {
+      setAvatar(e.target.files[0]);
+   };
 
-//    const handleUpload = async () => {
-//       if (!avatar) {
-//          alert("Please first select a file");
+   const handleUpload = () => {
+    console.log("Uploading");
+      if (!avatar) {
+         alert("Please first select a file");
          
-//          return;
-//       }
-// console.log(avatar,"i am avatar");
-//       const formData = new FormData();
-//       const config={
-
-//         headers: {
+         
+      }
+console.log(avatar,"i am avatar");
+      const formData = new FormData();
+      const config={
+        headers: {
   
-//           "Content-Type": "multipart/form-data",
+          "Content-Type": "multipart/form-data",
   
-//         },
+        },
   
-//        }
-//       formData.append("file", avatar);
+       }
+      formData.append("avatar", avatar);
 
-//       try {
-//          // Replace this URL with your server-side endpoint for handling file uploads
-//          const response = await fetch("http://localhost:5876/api/addcourt", {
-//             method: "POST",
-//             headers: config,
-//             body: formData
-//          });
+      try {
+        //  // Replace this URL with your server-side endpoint for handling file uploads
+        //  const response = await fetch("http://localhost:5876/api/addcourt", {
+        //     method: "POST",
+        //     headers: config,
+        //     body: formData
+        //  });
+        axios.post("http://localhost:5876/api/addcourt",formData,config).then((res:any)=>
+        {
+              console.log("success")
+              alert(" uploaded the file");
+    
+        }).catch((err:any)=>
+        {
+            console.log(err,"i m serve rror")
+        })
+        
 
-//          if (response.ok) {
-//             alert("File upload is  successfully");
-            
-//          } else {
-//             alert("Failed to upload the file due to errors");
-//          }
-//       } catch (error) {
-//          console.error("Error while uploading the file:", error);
-//          alert("Error occurred while uploading the file");
-//       }
-//    };
+      
+      } catch (error) {
+         console.error("Error while uploading the file:", error);
+         alert("Error occurred while uploading the file");
+      }
+   };
 
   
   return (
@@ -59,11 +65,11 @@ function CourtModal() {
         <Modal.Header />
         <Modal.Body>
           <div className="space-y-6">
-          {/* <div>
+          <div>
     
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-   </div> */}
+      <button onClick={()=>handleUpload()}>Upload</button>
+   </div>
           {/* <img className='h-full w-full ' src={require("../../assets/court.jpg")} alt=''/> */}
             <div>
               <div className="mb-2 block">
